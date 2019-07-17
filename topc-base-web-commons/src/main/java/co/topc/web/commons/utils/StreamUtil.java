a069005d073f80fc4fb4480b14ee57edb2dc0ac8
F-tao <1185766226@qq.com>
2019-07-17 12:57:48 +0800

    添加UUIDUtil、修改工具类

--- topc-base-web-commons/src/main/java/co/topc/web/commons/utils/StreamUtil.java
+++ topc-base-web-commons/src/main/java/co/topc/web/commons/utils/StreamUtil.java
@@ -10,24 +10,27 @@ import java.util.stream.Stream;
  * @author RuntimeExcepti0n
  * @date 2019/7/6 11:15
  */
-public class StreamUtil {
-    private StreamUtil(){}
+public final class StreamUtil {
+    private StreamUtil() {
+    }
 
     /**
      * 获取集合流，如果为空，返回空流
-     * @param collection  入参集合
+     *
+     * @param collection 入参集合
      * @return boolean
      */
-    public static <T> Stream<T> streamOf(Collection<T> collection){
-        return TopcCollectionUtils.isEmpty(collection) ? Stream.empty() :collection.stream();
+    public static <T> Stream<T> streamOf(Collection<T> collection) {
+        return TopcCollectionUtils.isEmpty(collection) ? Stream.empty() : collection.stream();
     }
 
     /**
      * 获取集合流
+     *
      * @param array 入参数组
-     * @return  stream 流
+     * @return stream 流
      */
-    public static <T> Stream<T> streamOf(T[] array){
-        return ArrayUtils.isEmpty(array) ? Stream.empty() :Arrays.stream(array);
+    public static <T> Stream<T> streamOf(T[] array) {
+        return ArrayUtils.isEmpty(array) ? Stream.empty() : Arrays.stream(array);
     }
 }
--- topc-base-web-commons/src/main/java/co/topc/web/commons/utils/TopcDateUtils.java
+++ topc-base-web-commons/src/main/java/co/topc/web/commons/utils/TopcDateUtils.java
@@ -1,13 +1,87 @@
 package co.topc.web.commons.utils;
 
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Topc 日期工具类
+ *
  * @author zaqweb
  * @date 2019-06-30 15:46
  */
-public final class TopcDateUtils {
+public final class TopcDateUtils extends DateUtils {
 
-  public static void main(String[] args) {
-    TopcStringUtils.isEmpty("");
-  }
+    public static final String DEFAULT_DATE_FORMAT = "yyyy/MM/dd";
+    public static final String DATE_SHORT_FORMAT = "yyyyMMdd";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DATE_CH_FORMAT = "yyyy年MM月dd日";
+
+    public static final String DATE_MONTH_FORMAT = "yyyy-MM";
+    public static final String TIME_FORMAT = "HH:mm:ss";
+
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy/MM/dd HH:mm:ss";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_TIME_CH_FORMAT = "yyyy年MM月dd日 HH:mm:ss";
+    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.S";
+
+    public static final String DAYTIME_START = "00:00:00";
+    public static final String DAYTIME_END = "23:59:59";
+
+    private static final String[] FORMATS = {"yyyy-MM-dd HH:mm", "yyyy-MM-dd HH:mm:ss",
+            "yyyy-MM-dd HH:mm:ss.S", "yyyy/MM/dd HH:mm:ss", "yyyy年MM月dd日 HH:mm:ss",
+            "yyyy-MM-dd", "yyyy/MM/dd", "yyyyMMdd", "yyyy年MM月dd日",
+            "HH:mm", "HH:mm:ss", "yyyy-MM"};
+
+    /**
+     * 日期转成指定格式的日期字符串
+     *
+     * @param date    日期
+     * @param pattern 日期格式
+     * @return
+     */
+    public static String dateToString(Date date, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        return sdf.format(date);
+    }
+
+    /**
+     * 字符串转成指定格式的日期字符串
+     *
+     * @param dateStr 字符串
+     * @param pattern 日期格式
+     * @return
+     * @throws ParseException
+     */
+    public static String dateToString(String dateStr, String pattern) throws ParseException {
+        Date date = stringToDate(dateStr);
+        return dateToString(date, pattern);
+    }
+
+    /**
+     * 字符串转成date
+     *
+     * @param dateStr 字符串
+     * @return
+     * @throws ParseException
+     */
+    public static Date stringToDate(String dateStr) throws ParseException {
+        return parseDate(dateStr, FORMATS);
+    }
+
+    /**
+     * 时间毫秒值转成date
+     *
+     * @param timeMillis 时间毫秒值
+     * @return
+     */
+    public static Date timeMillisToDate(Long timeMillis) {
+        return new Date(timeMillis);
+    }
+
+    public static void main(String[] args) {
+        TopcStringUtils.isEmpty("");
+    }
 }
